ee0fa2e7093acf8ab60e368024daba5df9e8364c
Robert Sato <robertssato@gmail.com>
2021-02-22 17:12:36 -0800

    added get_next_action helper function. also added convert_index for 3D->1D array. passes testing for get_next_action

--- c_version/q_learning.c
+++ c_version/q_learning.c
@@ -3,12 +3,34 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define NUM_ACTIONS 4
+int max_rows;
+int max_cols;
+
 // declare a pair called state for passing row and col of a given state
 typedef struct state {
     int row, col;
 } state;
 
 // helper functions
+// converting 3D/2D indices to 1D
+int convert_index(int dimensions, int row, int col, int depth) { // note: max_depth always = NUM_ACTIONS
+    int index;
+    if(dimensions == 3) {
+        index = depth + (col * NUM_ACTIONS) + (row * max_cols * NUM_ACTIONS);
+        printf("convert_index(): converted row: %d, col: %d, depth: %d to index: %d\n", row, col, depth, index);
+    }
+    else if(dimensions == 2) {
+        index = col + (row * col);
+        printf("convert_index(): converted row: %d, col: %d to index: %d\n", row, col, index);
+    }
+    else {
+        index = -1;
+        perror("invalid number of dimensions");
+    }
+    return index;
+}
+
 // checks if the given row, col state is terminal
 bool is_terminal(int values[], int row, int col);
 
@@ -21,9 +43,40 @@ state get_rand_start(int values[], int max_rows, int max_cols);
 //         start_row = np.random.randint(max_rows)
 //         start_col = np.random.randint(max_cols)
 //     return start_row, start_col
-// 
+
 // get the next action following an epsilon greedy choice
-int get_next_action(int q_table[][][], double epsilon, int row, int col);
+int get_next_action(int q_table[], double epsilon, int row, int col) {
+    int action_num, max_index, max, index;
+    action_num = max_index = max = -1;
+
+    float rand_val = (double)rand() / (double)RAND_MAX;
+    printf("random value in range 0 -> 1: %f\n", rand_val);
+    // take the greedy action
+    if(rand_val < epsilon) {
+        for(int i = 0; i < NUM_ACTIONS; i++) {
+            // find the max value
+            index = convert_index(3, row, col, i);
+            if(q_table[index] > max) {
+                max = q_table[index];
+                max_index = i;
+            }
+        }
+        // save the index of the max value as the best action
+        action_num = max_index;
+        printf("greedy action for row: %d, col: %d is: %d\n", row, col, action_num);
+    }
+    // take a random action
+    else {
+        action_num = rand() % 4;
+        printf("random action for row: %d, col: %d is: %d\n", row, col, action_num);;
+    }
+    if(action_num == -1) {
+        // still unset so error
+        perror("Error finding best action to take in get_next_action()\n");
+        return -1;
+    }
+    return action_num;
+}
 // def get_next_action(q_table, epsilon, row, col):
 //     # return greedy action
 //     if np.random.random() < epsilon:
@@ -32,7 +85,7 @@ int get_next_action(int q_table[][][], double epsilon, int row, int col);
 //     else:
 //         action_num = np.random.randint(num_actions)
 //     return action_num
-// 
+
 // get the next location. if invalid, don't move
 state get_next_location(int old_row, int old_col, int max_rows, int max_cols, int action_num);
 // def get_next_location(old_row, old_col, max_rows, max_cols, action_num):
@@ -50,9 +103,9 @@ state get_next_location(int old_row, int old_col, int max_rows, int max_cols, in
 //         new_col = old_col - 1
 //     # else invalid move -> dont move
 //     return new_row, new_col
-// 
+
 // get the best path following the Q-table
-state *get_best_path(int q_table[][][], int values[], int start_row, int start_col);
+state *get_best_path(int **q_table[], int values[], int start_row, int start_col);
 // def get_best_path(q_table, values, start_row, start_col):
 //     path = []
 //     curr_row, curr_col = start_row, start_col
@@ -64,7 +117,7 @@ state *get_best_path(int q_table[][][], int values[], int start_row, int start_c
 //     # append the terminal state as well
 //     path.append([curr_row, curr_col])
 //     return path
-// 
+
 int get_path_cost(int values[], state *path);
 //     cost = 0
 //     for row, col in path:
@@ -91,6 +144,7 @@ int main() {
     double epsilon, discount_factor, learning_rate, width, height;
     int param_num = 0;
     char *ptr;
+    printf("Printing contents of matrx_data.csv\n");
     while(fgets(buffer, sizeof(buffer), matrix_data)) {
         char *token;
 
@@ -105,7 +159,7 @@ int main() {
 
             // first row so add individual parameters
             if(row == 0) {
-                printf("Adding parameter");
+                //printf("Adding parameter");
                 switch(param_num) {
                     case 0 :
                        epsilon = strtod(token, &ptr);
@@ -118,9 +172,11 @@ int main() {
                        break;
                     case 3 :
                        width = strtod(token, &ptr);
+                       max_cols = width;
                        break;
                     case 4 :
                        height = strtod(token, &ptr);
+                       max_rows = height;
                        row += 1; // done with parameters so increase the row
                        break;
                 }
@@ -137,15 +193,38 @@ int main() {
     }
 
     // testing hyper parameters
-    printf("%f, %f, %f, %f, %f\n", epsilon, discount_factor, learning_rate, width, height);
+    printf("\nTesting hyper parameters:\n%f, %f, %f, %f, %f\n", epsilon, discount_factor, learning_rate, width, height);
 
     // setting and testing actions
     const char *actions[] = {"up", "right", "down", "left"};
-    printf("%s\n", actions[0]);
+    printf("Testing an action in actions array:\n%s\n", actions[0]);
 
     // testing state struct
     state my_state = {0, 0};
-    printf("%d, %d\n", my_state.row, my_state.col);
+    printf("Testing row, col of a state:\n%d, %d\n", my_state.row, my_state.col);
+
+    // used for epsilon
+    printf("rand: %d\nRAND_MAX: %d\n", rand(), RAND_MAX);
+    float rand_val = (double)rand() / (double)RAND_MAX;
+    printf("random value in range 0 -> 1: %f\n", rand_val);
+
+    // initialize 3D q_table: dim = (height, width, NUM_ACTIONS)
+    size_t nbytes = height * width * NUM_ACTIONS * sizeof(int);
+    // use flat 1D array for q_table implementation
+    int *q_table = (int*) malloc(nbytes);
+    memset(q_table, 0, nbytes);
+
+    // test get_next_action()
+    int action_num = get_next_action(q_table, epsilon, 0, 0);
+    printf("action_num = %d\n", action_num);
+    q_table[convert_index(3, 0, 0, 1)] = 10;
+    action_num = get_next_action(q_table, 1, 0, 0);
+    if(action_num == 1) {
+        printf("get_next_action test passed");
+    }
+    else {
+        printf("get_next_action test failed");
+    }
 
     return 0;
 }
