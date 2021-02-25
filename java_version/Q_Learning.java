1e8ce212bd67510a0bc30d4f180c97f087e07b57
Robert Sato <robertssato@gmail.com>
2021-02-24 16:31:51 -0800

    working get_rand_start. get_next_action not tested

--- java_version/Q_Learning.java
+++ java_version/Q_Learning.java
@@ -17,6 +17,49 @@ public class Q_Learning
         }
     }
 
+    // gets a random valid start state; max row and cols are stored as globals in Parameters.java
+    private static State get_rand_start(int[][] values) {
+        // create instance of Random class
+        Random rand = new Random();
+        int start_row = rand.nextInt(Parameters.MAX_ROWS);
+        int start_col = rand.nextInt(Parameters.MAX_COLS);
+        while(is_terminal(values, start_row, start_col)) {
+            start_row = rand.nextInt(Parameters.MAX_ROWS);
+            start_col = rand.nextInt(Parameters.MAX_COLS);
+        }
+        State start = new State(start_row, start_col);
+        return start;
+    }
+
+    // get the next action following an epsilon greedy choice
+    private static int get_next_action(int[][][] q_table, int row, int col) {
+        Random rand = new Random();
+        double eps_rand = rand.nextDouble();
+        System.out.println("eps_rand = " + eps_rand);
+        int action_num, index;
+        int max_index = -1;
+        int max = -1;
+        if(eps_rand < Parameters.EPSILON) {
+            // find the max value
+            for(int i = 0; i < Parameters.num_actions; i++) {
+                if(q_table[row][col][i] > max) {
+                    max = q_table[row][col][i];
+                    max_index = i;
+                }
+            }
+            action_num = max_index;
+        }
+        else {
+            action_num = rand.nextInt(4);
+        }
+        if(action_num == -1) {
+            // still unset so error
+            System.out.error("Error getting next action");
+            return -1;
+        }
+        return action_num;
+    }
+
     public static void main(String[] args)   
     {  
         String line = "";  
@@ -84,23 +127,24 @@ public class Q_Learning
         // testing State.java
         State temp = new State(0, 1);
         System.out.println("temp = (" + temp.getRow() + ", " + temp.getCol() + ")");
-    
-        // test is_terminal
-        if(is_terminal(values, temp.getRow(), temp.getCol())) {
-            System.out.println("temp is terminal!");
-        }
-        else {
-            System.out.println("temp is not terminal!");
-        }
-
         temp.setRow(2);
         System.out.println("temp = (" + temp.getRow() + ", " + temp.getCol() + ")");
-
+    
+        // test is_terminal
+        System.out.println("Testing is_terminal:");
         if(is_terminal(values, temp.getRow(), temp.getCol())) {
             System.out.println("temp is terminal!");
         }
         else {
             System.out.println("temp is not terminal!");
+        }
+
+        // test get_rand_start
+        System.out.println("Testing get_rand_start:");
+        State start = get_rand_start(values);
+        for(int i = 0; i < 10; i++) {
+            System.out.println("start = (" + start.getRow() + ", " + start.getCol() + ")");
+            start = get_rand_start(values);
         }
     }  
 } 
